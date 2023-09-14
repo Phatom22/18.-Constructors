2108fdb0a754467bc658118928748a3105bbee4e
nomvula <nzshabangu95@gmail.com>
2023-09-14 10:57:11 +0200

    Add project files.

---
+++ 18. Constructors/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        //contructor
+        public Book(string aTitle, string aAauthor, int aPages)
+        {
+            title = aTitle;
+            author = aAauthor;
+            pages = aPages;
+        }
+    }
+}
---
+++ 18. Constructors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("harry potter", "jerry.r", 400);
+            Book book2 = new Book("potter","jerry", 500);
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book2.author);
+            Console.ReadLine();
+        }
+    }
+}
